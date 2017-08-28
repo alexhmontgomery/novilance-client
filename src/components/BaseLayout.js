f1d73742d202ac9b49b2f16631c0f871d0230c60
Alex <alexhmontgomery@gmail.com>
2017-08-28 15:46:56 -0500

    initial project commit

---
+++ src/components/BaseLayout.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default class BaseLayout extends Component {
+  render () {
+    return (
+      <div>
+        <nav>
+          <NavLink exact to='/' id='home-link'>novilance</NavLink>
+          <span id='nav-spacer' />
+          <div className='navbar-link-group'>
+            <NavLink to='/projects' className='navbar-horizontal-item'>Projects</NavLink>
+            <NavLink to='/freelancers' className='navbar-horizontal-item'>Freelancers</NavLink>
+            <div className='navbar-horizontal-item' id='navbar-vertical-section'>
+              <div className='navbar-vertical-item' id='top-vert-navbar-item'>
+                <NavLink to='/authenticate'>Login</NavLink>
+              </div>
+              <div className='navbar-vertical-item'>
+                <NavLink to='/register'>Sign Up Free</NavLink>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        { this.props.children }
+
+        <footer>
+          Insert footer here.
+        </footer>
+      </div>
+    )
+  }
+}
